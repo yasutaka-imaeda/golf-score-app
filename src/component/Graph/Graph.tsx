cf00876239322341fec8e9d78e932573e3cf1a57
yasutaka.imaeda <yasu89baka1013@icloud.com>
2022-03-15 19:17:23 +0900

    簡易グラフの作成

---
+++ src/component/Graph/Graph.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { useAppSelector } from "../../app/hooks";
+import { selectScoreList } from "../../app/scoreSlice";
+import styles from "./Graph.module.scss";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Label,
+} from "recharts";
+
+const Graph: React.FC = () => {
+  const setScoreList = useAppSelector(selectScoreList);
+  console.log(setScoreList);
+  let data: any;
+  const scoreList = setScoreList.map((item: any) => {
+    console.log(item);
+    if (item.id !== undefined) {
+      return {
+        scoreData: JSON.parse(item.score),
+        sumScore: item.sumScore,
+        sumPat: item.sumPat,
+        scoreCreatedAt: item.createdAt.substring(5, 10),
+      };
+    } else {
+      return {};
+    }
+  });
+  console.log(scoreList);
+  data = scoreList.map((item: any) => {
+    return { create: item.scoreCreatedAt, score: item.sumScore };
+  });
+  console.log(data);
+
+  return (
+    <div className={styles.root}>
+      {data !== null ? (
+        <LineChart
+          width={900}
+          height={600}
+          data={data}
+          margin={{ top: 5, right: 20, bottom: 24, left: 20 }}
+        >
+          <Line type="monotone" dataKey="score" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+          <XAxis dataKey="create">
+            <Label value="日付" offset={0} position="bottom" />
+          </XAxis>
+          <YAxis
+            label={{ value: "スコア", angle: -90, position: "insideLeft" }}
+          />
+          <Tooltip />
+        </LineChart>
+      ) : (
+        <div>データがありません</div>
+      )}
+    </div>
+  );
+};
+
+export default Graph;
--- src/pages/Analysis/Analysis.tsx
+++ src/pages/Analysis/Analysis.tsx
@@ -1,26 +1,17 @@
 import React from "react";
-import { useAppSelector } from "../../app/hooks";
-import { selectScoreList } from "../../app/scoreSlice";
+import Graph from "../../component/Graph/Graph";
 import Header from "../../component/Header/Header";
 import styles from "./Analysis.module.scss";
 
 const Analysis: React.FC = () => {
-  const setScoreList = useAppSelector(selectScoreList);
-  const scoreList = setScoreList.map((item: any) => {
-    return {
-      scoreData: JSON.parse(item.score),
-      sumScore: item.sumScore,
-      sumPat: item.sumPat,
-      scoreCreatedAt: item.createdAt.substring(5, 10),
-    };
-  });
-  console.log(scoreList);
-
   return (
     <div className={styles.root}>
       <div className={styles.container}>
         <div className={styles.header}>
           <Header />
+        </div>
+        <div className={styles.graph}>
+          <Graph />
         </div>
       </div>
     </div>
