d6a772af5fb7031c94d8ece184f596df3c0e0ee1
yasutaka.imaeda <yasu89baka1013@icloud.com>
2022-03-23 10:43:57 +0900

    カレンダーの日本語表示に変更

--- src/component/Calender/Calender.tsx
+++ src/component/Calender/Calender.tsx
@@ -1,19 +1,23 @@
 import React, { useState } from "react";
 import styles from "./Calender.module.scss";
 import { useAppDispatch } from "../../app/hooks";
-import DatePicker from "react-datepicker";
+import DatePicker, { registerLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import ja from "date-fns/locale/ja";
 
 const Calender: React.FC = () => {
   const dispatch = useAppDispatch();
+  registerLocale("ja", ja);
   const [startDate, setStartDate] = useState(new Date());
 
   return (
     <div className={styles.root}>
-      <div className={styles.title}>コースを回った日付</div>
+      <div className={styles.title}>プレー日</div>
       <DatePicker
         selected={startDate}
         onChange={(date: Date) => setStartDate(date)}
+        dateFormat="yyyy/MM/dd"
+        locale="ja"
       />
     </div>
   );
